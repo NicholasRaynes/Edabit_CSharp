cf1be0ec1a338ca9a1d8671fdebeacc405d72618
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-02-05 19:14:19 -0600

    Completed documentation.

--- LetterDCounter.cs
+++ LetterDCounter.cs
@@ -1,9 +1,26 @@
+/*
+ * Name: Nicholas Raynes
+ * Version: 0.0.1
+ * Source: https://edabit.com/challenge/kMWmiNJM4szSv7dLd
+ */
+
 using System;
 
 namespace GitHubSandbox
 {
+    /// <summary>
+    /// This program includes the declaration, implementation,
+    /// and testing for the LetterDCounter method.
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// This method counts and returns how often the letter 
+        /// "d" occurs in a phrase, regardless of case.
+        /// </summary>
+        /// <param name="phrase">The phrase to be scanned for how many letter 
+        /// d's it contains</param>
+        /// <returns>Number of letter d's within the phrase.</returns>
         public static int LetterDCounter(string phrase)
         {
             int count = 0;
