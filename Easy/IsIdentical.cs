66d6a30e19c4c3d43b50230c8059a3b6ab972efe
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-02-09 14:57:00 -0600

    Added some tests for the IsIdentical method.

--- Easy/IsIdentical.cs
+++ Easy/IsIdentical.cs
@@ -4,6 +4,23 @@ namespace GitHubSandbox
 {   
     internal class Program
     {
+        public static void Main(string[] args)
+        {
+            // Testing
+            string test1 = "aaaaaa";
+            string test2 = "aabaaa";
+            string test3 = "ccccca";
+            string test4 = "kk";
+
+            Console.WriteLine(IsIdentical(test1));
+            Console.WriteLine(IsIdentical(test2));
+            Console.WriteLine(IsIdentical(test3));
+            Console.WriteLine(IsIdentical(test4));
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+        
         public static bool IsIdentical(string str)
         {
             char firstCharacter = str[0];
