513d6d4768ff338a3ead17ba8f5322e46570fbea
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-02-16 18:39:53 -0600

    Added some testing for the method.

--- Medium/ArrayOfMultiplesProgram.cs
+++ Medium/ArrayOfMultiplesProgram.cs
@@ -4,6 +4,16 @@ namespace GitHubSandbox
 {
     public class ArrayOfMultiplesProgram
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test #1 - ArrayOfMultiples(7, 5)\n{0}\n", String.Join(", ", ArrayOfMultiples(7, 5)));
+            Console.WriteLine("Test #2 - ArrayOfMultiples(12, 10)\n{0}\n", String.Join(", ", ArrayOfMultiples(12, 10)));
+            Console.WriteLine("Test #3 - ArrayOfMultiples(17, 6)\n{0}\n", String.Join(", ", ArrayOfMultiples(17, 6)));
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
         public static int[] ArrayOfMultiples(int num, int length)
         {
             int[] multiples = new int[length];
