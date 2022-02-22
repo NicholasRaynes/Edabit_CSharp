1278de20127bdc3e1e92784787d5de4150e01d37
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-02-21 21:45:26 -0600

    Added some testing for the method.

--- Medium/ReverseCaseProgram.cs
+++ Medium/ReverseCaseProgram.cs
@@ -4,6 +4,17 @@ namespace GitHubSandbox
 {
 	public class ReverseCaseProgram
 	{
+		public static void Main(string[] args)
+		{
+			// Testing
+			Console.WriteLine("Test #1 - ReverseCase('Happy Birthday')\n{0}\n", ReverseCase("Happy Birthday"));
+			Console.WriteLine("Test #2 - ReverseCase('MANY THANKS')\n{0}\n", ReverseCase("MANY THANKS"));
+			Console.WriteLine("Test #3 - ReverseCase('sPoNtAnEoUs')\n{0}\n", ReverseCase("sPoNtAnEoUs"));
+
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
+		}
+		
 		public static string ReverseCase(string input)
 		{
 			string result = "";
