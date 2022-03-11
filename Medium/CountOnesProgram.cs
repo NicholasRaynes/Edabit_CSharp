5549ffc3da9c904da15285725d26492ef41f1c6a
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-03-10 19:39:18 -0600

    Create CountOnesProgram.cs

---
+++ Medium/CountOnesProgram.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+
+namespace GitHubSandbox
+{
+	public class CountOnesProgram
+    {
+		public static int CountOnes(int i)
+		{
+			string binary = Convert.ToString(i, 2);
+
+			int count = 0;
+
+			foreach (char c in binary)
+			{
+				if (c == '1')
+				{
+					count++;
+				}
+			}
+			return count;
+		}
+	}
+}
