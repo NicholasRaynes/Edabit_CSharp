b4ad9d51dd8487755ed755a6be197439f1107d86
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-03-10 19:43:35 -0600

    Added some testing for the method.

--- Medium/CountOnesProgram.cs
+++ Medium/CountOnesProgram.cs
@@ -5,6 +5,17 @@ namespace GitHubSandbox
 {
     public class CountOnesProgram
     {
+	public static void Main(string[] args)
+	{
+		// Testing
+		Console.WriteLine("Test #1 - CountOnes(0)\n{0}\n", CountOnes(0));
+		Console.WriteLine("Test #2 - CountOnes(100)\n{0}\n", CountOnes(100));
+		Console.WriteLine("Test #3 - CountOnes(999)\n{0}\n", CountOnes(999));
+
+		Console.WriteLine("Press any key to exit...");
+		Console.ReadKey();
+	}
+	    
 	public static int CountOnes(int i)
 	{
 		string binary = Convert.ToString(i, 2);
