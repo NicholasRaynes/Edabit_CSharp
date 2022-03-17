5a263928ce59cb6a6ab83e5559a2683b45d90334
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-03-17 12:09:43 -0500

    Completed some tests for the method.

--- Hard/ReverseAndNotProgram.cs
+++ Hard/ReverseAndNotProgram.cs
@@ -4,6 +4,17 @@ namespace GitHubSandbox
 {
     public class ReverseAndNotProgram
     {
+        public static void Main(string[] args)
+        {
+            // Testing
+            Console.WriteLine("Test #1 - ReverseAndNot(123)\n{0}\n", ReverseAndNot(123));
+            Console.WriteLine("Test #2 - ReverseAndNot(152)\n{0}\n", ReverseAndNot(152));
+            Console.WriteLine("Test #3 - ReverseAndNot(123456789)\n{0}\n", ReverseAndNot(123456789));
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+        
         public static string ReverseAndNot(int i)
         {
             string input = i.ToString();
