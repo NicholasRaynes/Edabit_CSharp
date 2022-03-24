199b4187b9ee8f8ce65edfa2e4be152a90efaf31
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-03-23 19:18:39 -0500

    Completed some testing for the method.

--- Hard/WeekAfterProgram.cs
+++ Hard/WeekAfterProgram.cs
@@ -6,6 +6,17 @@ namespace GitHubSandbox
 {
     public class WeekAfterProgram
     {
+        public static void Main(string[] args)
+        {
+            // Testing
+            Console.WriteLine("Test #1 - WeekAfter('12/03/2020')\n{0}\n", WeekAfter("12/03/2020"));
+            Console.WriteLine("Test #2 - WeekAfter('21/12/1989')\n{0}\n", WeekAfter("21/12/1989"));
+            Console.WriteLine("Test #3 - WeekAfter('01/01/2000')\n{0}\n", WeekAfter("01/01/2000"));
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+        
         public static string WeekAfter(string date)
         {
             DateTime d = new DateTime();
