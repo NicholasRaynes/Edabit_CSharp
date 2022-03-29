fdf39f2f20302bd054e7c54ba687fd3bf260156f
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-03-29 18:03:39 -0500

    Create ConsecutiveNumbersProgram.cs

---
+++ Hard/ConsecutiveNumbersProgram.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace GitHubSandbox
+{
+  public class ConsecutiveNumbersProgram
+  {
+    public static bool ConsecutiveNumbers(int[] arr)
+    {
+      Array.Sort(arr);
+      
+      for (int i = 0; i < arr.Length - 1; i++)
+			{
+				if (arr[i] + 1 != arr[i + 1])
+				{
+					return false;
+				}
+			}
+      
+      return true;
+    }
+  }
+}
