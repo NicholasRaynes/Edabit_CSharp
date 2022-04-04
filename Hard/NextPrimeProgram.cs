cc71a4423d236207e2c51388d547055288115b97
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-04-04 15:25:17 -0500

    Completed some tests for the method.

--- Hard/NextPrimeProgram.cs
+++ Hard/NextPrimeProgram.cs
@@ -4,6 +4,17 @@ namespace GitHubSandbox
 {
     public class NextPrimeProgram
     {
+        public static void Main(String[] args)
+        {
+            // Testing
+            Console.WriteLine("Test #1 - NextPrime(12)\n{0}\n", NextPrime(12));
+            Console.WriteLine("Test #2 - NextPrime(24)\n{0}\n", NextPrime(24));
+            Console.WriteLine("Test #3 - NextPrime(11)\n{0}\n", NextPrime(11));
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+        
         public static int NextPrime(int num)
         {
             int result = 1;
